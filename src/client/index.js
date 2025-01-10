2b51d67ac16bf365504a114e2e62db1bf996176c
Chukwunenye Onyedikachi Flavian <chuksoflavian@gmail.com>
2025-01-10 09:17:35 +0100

    packages installation and correction

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
+import App from '../App.js'; // Add the .js extension
 
-ReactDOM.hydrate(<App todos={window.__INITIAL_DATA__} />, document.getElementById('root'));
+ReactDOM.hydrate(<App todos={window.__INITIAL_DATA__} />, document.getElementById('root'));*/
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client'; // Use hydrateRoot for React 18
+import App from '../App.js';
+
+const root = document.getElementById('root');
+hydrateRoot(root, <App />);
